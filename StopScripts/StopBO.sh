29209f5b0a8147389b0205ce077cb2afd9ee9191
Nagarjuna Jammula <75269671+nagarjuna-jammula@users.noreply.github.com>
2024-10-03 14:31:09 +0200

    Update StopBO.sh

--- StopScripts/StopBO.sh
+++ StopScripts/StopBO.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #Stop Server
-sudo su - bopadm -c '/usr/sap/BOP/sap_bobj/stopservers'
+sudo su - <sid>adm -c '/usr/sap/<SID>/sap_bobj/stopservers'
 #Stop Tomcat
-sudo su - bopadm -c '/usr/sap/BOP/sap_bobj/tomcatshutdown.sh'
+sudo su - <sid>adm -c '/usr/sap/<SID>/sap_bobj/tomcatshutdown.sh'
 #Stop SQL
-sudo su - bopadm -c '/usr/sap/BOP/sap_bobj/sqlanywhere_shutdown.sh'
+sudo su - <sid>adm -c '/usr/sap/<SID>/sap_bobj/sqlanywhere_shutdown.sh'
 sleep 30
 
 #Checking if BO is Stopped based on the process count
 
-BOPROC=`ps -ef | grep bopadm | grep BOP | grep -v grep | wc -l`
-TOMCATPROC=`ps -ef | grep bopadm | grep tomcat | grep -v grep | wc -l`
-SQLPROC=`ps -ef | grep bopadm | grep sqlanywhere | grep -v grep | wc -l`
+BOPROC=`ps -ef | grep <sid<adm | grep <SID> | grep -v grep | wc -l`
+TOMCATPROC=`ps -ef | grep <sid>adm | grep tomcat | grep -v grep | wc -l`
+SQLPROC=`ps -ef | grep <sid>adm | grep sqlanywhere | grep -v grep | wc -l`
 
 #Condition to check there are no running processes
 if [[ $BOPROC -eq 0 ]] && [[ $SQLPROC -eq 0 ]] && [[ $TOMCATPROC -eq 0 ]];
